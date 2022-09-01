beadece7f949ab8bc983d9fbaec663b8414d18b8
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-01 09:01:13 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  roots: ["<rootDir>/src", "<rootDir>/tests"],
+  roots: ["<rootDir>/src" /*, "<rootDir>/tests"*/],
   collectCoverageFrom: [
     "<rootDir>/src/**/*.{ts,tsx}",
     "!<rootDir>/src/main/**/*",
@@ -7,7 +7,7 @@ module.exports = {
     "!**/*.d.ts",
   ],
   coverageDirectory: "coverage",
-  setupFilesAfterEnv: ["<rootDir>/src/main/config/jest-setup.ts"],
+  //   setupFilesAfterEnv: ["<rootDir>/src/main/config/jest-setup.ts"],
   testPathIgnorePatterns: [
     "<rootDir>/node_modules/",
     "<rootDir>/tests/e2e/cypress",
