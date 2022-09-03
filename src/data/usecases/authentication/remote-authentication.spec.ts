ed4ffb19fcf8f606b4fbb28b4b7fc96ffad4a2f2
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-03 08:54:42 -0300

    test: ensure RemoteAuthentication calls HttpPostClient with correct URL

---
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -0,0 +1,19 @@
+import { HttpPostClient } from "../../protocols/http/http-post-client";
+import { RemoteAuthentication } from "./remote-authentication";
+
+describe("RemoteAuthentication", () => {
+  test("Should call HttpPostClient with correct URL", async () => {
+    class HttpPostClientSpy implements HttpPostClient {
+      url?: string;
+      async post(url: string): Promise<void> {
+        this.url = url;
+        return Promise.resolve();
+      }
+    }
+    const httpPostClient = new HttpPostClientSpy();
+    const url = "any_url";
+    const sut = new RemoteAuthentication(url, httpPostClient);
+    await sut.auth();
+    expect(httpPostClient.url).toBe(url);
+  });
+});
