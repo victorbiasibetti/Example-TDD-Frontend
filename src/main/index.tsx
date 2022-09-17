167474317cb55373c247b6980899f26e385a1bf4
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-17 11:48:29 -0300

    refactor: add index file with multiple imports

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,6 +2,6 @@ import React from "react";
 
 import ReactDOM from "react-dom";
 
-import Login from "@/presentation/pages/login/login";
+import { Login } from "@/presentation/pages";
 
 ReactDOM.render(<Login />, document.getElementById("main"));
