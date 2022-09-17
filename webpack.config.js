6b04dca0fcdeb72602c6ba07b664ddf74969d8f3
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-17 10:19:47 -0300

    chore: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -41,8 +41,10 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: "./public",
-    writeToDisk: true,
+    static: "./public",
+    devMiddleware: {
+      writeToDisk: true,
+    },
     historyApiFallback: true,
   },
   externals: {
