125bcb9bc171024bef384d1b0625cf7e5e9e2484
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-21 07:53:59 -0300

    test: ensure Login present error if email is invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -16,6 +16,7 @@ type SutTypes = {
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy();
+  validationSpy.errorMessage = faker.random.words();
   const sut = render(<Login validation={validationSpy} />);
   return { sut, validationSpy };
 };
@@ -24,7 +25,7 @@ describe("Login Component", () => {
   afterEach(cleanup);
 
   test("Should start with initial state", () => {
-    const { sut } = makeSut();
+    const { sut, validationSpy } = makeSut();
     const errorWrap = sut.getByTestId("error-wrap");
     expect(errorWrap.childElementCount).toBe(0);
 
@@ -32,7 +33,7 @@ describe("Login Component", () => {
     expect(submitButton.disabled).toBe(true);
 
     const emailStatus = sut.getByTestId("email-status");
-    expect(emailStatus.title).toBe("Campo obrigatório");
+    expect(emailStatus.title).toBe(validationSpy.errorMessage);
     expect(emailStatus.textContent).toBe("🔴");
 
     const passwordStatus = sut.getByTestId("password-status");
@@ -56,5 +57,14 @@ describe("Login Component", () => {
     fireEvent.input(passwordInput, { target: { value: password } });
     expect(validationSpy.fieldName).toBe("password");
     expect(validationSpy.fieldValue).toBe(password);
+  });
+
+  test("Should show email error if validation fails", () => {
+    const { sut, validationSpy } = makeSut();
+    const emailInput = sut.getByTestId("email");
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } });
+    const emailStatus = sut.getByTestId("email-status");
+    expect(emailStatus.title).toBe(validationSpy.errorMessage);
+    expect(emailStatus.textContent).toBe("🔴");
   });
 });
