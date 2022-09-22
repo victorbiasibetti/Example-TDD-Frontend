d070e5be2b7b1862d416a391425dae3f422534e2
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-22 07:55:22 -0300

    feat: prevent Login to submit form if its invalid

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -37,7 +37,7 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     event: React.FormEvent<HTMLFormElement>
   ): Promise<void> => {
     event.preventDefault();
-    if (state.isLoading) return;
+    if (state.isLoading || state.emailError || state.passwordError) return;
     setState({ ...state, isLoading: true });
     await authentication.auth({ email: state.email, password: state.password });
   };
@@ -46,7 +46,11 @@ const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
     <div className={Styles.login}>
       <LoginHeader />
       <Context.Provider value={{ state, setState }}>
-        <form className={Styles.form} onSubmit={handleSubmit}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
           <h2>Login</h2>
           <Input type="email" name="email" placeholder="Digite seu e-mail" />
           <Input
