898e6bc4c0e8d9a344dfe7148e9c09a5efd37daf
Victor Biasibetti <vbiasibetti@universe.dart.spb@vbiasibetti.nb.ipa.dataart.net>
2022-09-22 07:55:38 -0300

    test: prevent Login to submit form if its invalid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -152,4 +152,15 @@ describe("Login Component", () => {
 
     expect(authenticationSpy.callCount).toBe(1);
   });
+
+  test("Should not call Authentication if form is invalid", () => {
+    const validationError = faker.random.words();
+    const { sut, authenticationSpy } = makeSut({
+      validationError,
+    });
+    populateEmailField(sut);
+
+    fireEvent.submit(sut.getByTestId("form"));
+    expect(authenticationSpy.callCount).toBe(0);
+  });
 });
