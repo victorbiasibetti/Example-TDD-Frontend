2026dc6ae3f232f7b565498506397286d329e0a1
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-09-23 08:45:02 -0300

    feat: ensure EmailValidation returns error when email is invalid

---
+++ src/validation/errors/invalid-field-error.ts
@@ -0,0 +1,5 @@
+export class InvaliedFieldError extends Error {
+  constructor() {
+    super(`Valor inválido`);
+  }
+}
---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,9 @@
+import { InvaliedFieldError } from "@/validation/errors/invalid-field-error";
+import { FieldValidation } from "@/validation/protocols/field-validation";
+
+export class EmailValidation implements FieldValidation {
+  constructor(readonly field: string) {}
+  validate(value: string): Error {
+    return new InvaliedFieldError();
+  }
+}
