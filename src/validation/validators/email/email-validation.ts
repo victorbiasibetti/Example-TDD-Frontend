725df40264115264a1988c9524c69367570645ea
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-09-23 08:51:19 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -4,6 +4,11 @@ import { FieldValidation } from "@/validation/protocols/field-validation";
 export class EmailValidation implements FieldValidation {
   constructor(readonly field: string) {}
   validate(value: string): Error {
-    return new InvaliedFieldError();
+    const emailRegex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const isValid = String(value).toLowerCase().match(emailRegex);
+
+    return isValid ? null : new InvaliedFieldError();
   }
 }
