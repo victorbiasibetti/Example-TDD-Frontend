24fab19a6931ddfb607efab7b182b0ceb70fcf17
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-09-28 09:08:16 -0300

    feat: ensure add SaveAccessToken to Login presentation

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { makeLoginValidation } from './login-validation-factory'
 import { Login } from '@/presentation/pages'
+import { makeLoginValidation } from './login-validation-factory'
 import { makeRemoteAuthentication } from '@/main/factories/usecases/authentication/remote-authentication-factory'
+import { makeLocalSaveAccessToken } from '@/main/factories/usecases/save-access-token/local-save-acess-token-factory'
 
 export const makeLogin: React.FC = () => {
   return (
     <Login
       authentication={makeRemoteAuthentication()}
       validation={makeLoginValidation()}
+      saveAccessToken={makeLocalSaveAccessToken()}
     />
   )
 }
