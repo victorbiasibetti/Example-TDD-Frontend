66c783d5c020b572749a549d82dbb1c29e9866b3
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-01 08:19:40 -0300

    refactor: move folders to correct path

--- src/main/factories/usecases/authentication/remote-authentication-factory.ts
+++ src/main/factories/usecases/authentication/remote-authentication-factory.ts
@@ -1,7 +1,7 @@
 import { RemoteAuthentication } from '@/data/usecases/authentication/remote-authentication'
 import { Authentication } from '@/domain/usecases'
-import { makeApiUrl } from '@/main/factories/pages/http/api-url-factory'
-import { makeAxiosHttpClient } from '@/main/factories/pages/http/axios-http-client-factory'
+import { makeApiUrl } from '@/main/factories/http/api-url-factory'
+import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
 
 export const makeRemoteAuthentication = (): Authentication => {
   return new RemoteAuthentication(makeApiUrl('/login'), makeAxiosHttpClient())
