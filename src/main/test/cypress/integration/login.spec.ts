b1b08030ad3b8e2e42857a965e0e7b8dbdcba266
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-04 08:29:51 -0300

    test: ensure login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,15 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('Should load with correct initial state', () => {
+    cy.get('[data-testid="email-status"]')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.get('[data-testid="password-status"]')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
+    cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
   })
 })
