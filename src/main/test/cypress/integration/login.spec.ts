ba12f948717d5f993374a412cf94aa26e08833fd
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-05 08:44:26 -0300

    test: ensure Login will not submit form if its invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -130,4 +130,16 @@ describe('Login', () => {
     cy.get('[data-testid="submit"]').dblclick()
     cy.get('@request.all').should('have.length', 1)
     })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept('POST', /login/, {
+      statusCode: 200,
+      body: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+    cy.get('[data-testid="email"]')
+      .type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+    })
 })
