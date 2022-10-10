dea23b32017fb0219277dacb9b01214e1c99bce8
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-10 07:19:34 -0300

    test: ensure AxiosHttpClient returns correct value on success

--- src/infra/test/mock-axios.ts
+++ src/infra/test/mock-axios.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 import faker from 'faker'
 
 export const mockHttpResponse = (): any => ({
-  data: faker.random.objectElement(),
-  status: faker.datatype.number()
+  body: faker.random.objectElement(),
+  statusCode: faker.datatype.number()
 })
 
 export const mockAxios = (): jest.Mocked<typeof axios> => {
