6c642a785570e339f73025a693e3eae7609c5d2a
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-14 07:46:14 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,6 +21,42 @@ const SurveyList: React.FC = () => {
           </div>
           <footer>Ver Resultado</footer>
         </li>
+        <li>
+          <div className={Styles.surveyContent}>
+            <Icon iconName={IconName.thumbUp} className={Styles.iconWrap}/>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>10</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>Qual é seu framework web favorito?</p>
+          </div>
+          <footer>Ver Resultado</footer>
+        </li>
+        <li>
+          <div className={Styles.surveyContent}>
+            <Icon iconName={IconName.thumbUp} className={Styles.iconWrap}/>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>10</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>Qual é seu framework web favorito?</p>
+          </div>
+          <footer>Ver Resultado</footer>
+        </li>
+        <li>
+          <div className={Styles.surveyContent}>
+            <Icon iconName={IconName.thumbUp} className={Styles.iconWrap}/>
+            <time>
+              <span className={Styles.day}>22</span>
+              <span className={Styles.month}>10</span>
+              <span className={Styles.year}>2022</span>
+            </time>
+            <p>Qual é seu framework web favorito?</p>
+          </div>
+          <footer>Ver Resultado</footer>
+        </li>
 
       </ul>
     </div>
