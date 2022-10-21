f414dad200a9718a18a1580c335c4deb8a0b301a
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-21 07:22:08 -0300

    refactor: move Survey List Items to components

---
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -0,0 +1,22 @@
+import { Icon, IconName } from '@/presentation/components'
+import React from 'react'
+import Styles from './survey-item-styles.scss'
+
+const SurveyItem: React.FC = () => {
+  return (
+    <li className={Styles.surveyItemWrap}>
+      <div className={Styles.surveyContent}>
+        <Icon iconName={IconName.thumbUp} className={Styles.iconWrap}/>
+        <time>
+          <span className={Styles.day}>22</span>
+          <span className={Styles.month}>10</span>
+          <span className={Styles.year}>2022</span>
+        </time>
+        <p>Qual é seu framework web favorito?</p>
+      </div>
+      <footer>Ver Resultado</footer>
+    </li>
+  )
+}
+
+export default SurveyItem
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,4 +1,4 @@
-import { Footer, Header, Icon, IconName } from '@/presentation/components'
+import { Footer, Header } from '@/presentation/components'
 import Styles from './survey-list-styles.scss'
 import React from 'react'
 
@@ -9,22 +9,6 @@ const SurveyList: React.FC = () => {
     <div className={Styles.contentWrap}>
       <h2>Enquetes</h2>
       <ul>
-        <li>
-          <div className={Styles.surveyContent}>
-            <Icon iconName={IconName.thumbUp} className={Styles.iconWrap}/>
-            <time>
-              <span className={Styles.day}>22</span>
-              <span className={Styles.month}>10</span>
-              <span className={Styles.year}>2022</span>
-            </time>
-            <p>Qual é seu framework web favorito?</p>
-          </div>
-          <footer>Ver Resultado</footer>
-        </li>
-        <li>
-
-        </li>
-
       </ul>
     </div>
     <Footer />
