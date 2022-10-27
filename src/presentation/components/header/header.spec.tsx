3668e36e2fa9874778996e722d8b893def9a4cf1
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-10-27 08:17:22 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -1,3 +1,4 @@
+import { AccountModel } from '@/domain/models'
 import { Header } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
 import { fireEvent, render, screen } from '@testing-library/react'
@@ -9,17 +10,25 @@ jest.mock('react-router-dom', () => ({
   useNavigate: () => mockUseNavigate
 }))
 
+type SutTypes = {
+  setCurrectAccountMock: (account: AccountModel) => void
+}
+
+const makeSut = (): SutTypes => {
+  const setCurrectAccountMock = jest.fn()
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrectAccountMock }}>
+      <Header />
+    </ApiContext.Provider>
+  )
+  return { setCurrectAccountMock }
+}
+
 describe('Header Component', () => {
   test('Should call setCurrentAccount with null', () => {
-    const setCurrectAccountMock = jest.fn()
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrectAccountMock }}>
-        <Header />
-      </ApiContext.Provider>
-    )
+    const { setCurrectAccountMock } = makeSut()
     fireEvent.click(screen.getByTestId('logout'))
     expect(setCurrectAccountMock).toHaveBeenCalledWith(undefined)
-
     expect(mockUseNavigate).toHaveBeenCalledWith('/login', { replace: true })
   })
 })
