7c083355b842746ae4da44733561bdadfb1d7edf
Victor Biasibetti <victorbiasibetti7@gmail.com>
2022-12-26 08:51:27 -0300

    feat: add SurveyResult layout

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
 import Styles from './survey-result-styles.scss'
+import { Header, Footer, Spinner } from '@/presentation/components'
+import FlipMove from 'react-flip-move'
+import React from 'react'
 
 const SurveyResult: React.FC = () => {
   return (
     <div className={Styles.surveyResultWrap}>
-      Teste
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>Pergunta</h2>
+        <FlipMove className={Styles.anwserList}>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li className={Styles.active}>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button>Voltar</button>
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Aguarde...</span>
+            <Spinner isNegative/>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   )
 }
