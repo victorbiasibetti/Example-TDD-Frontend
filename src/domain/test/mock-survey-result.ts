3a172e7c061f2597a35430275a5940c4c4244b17
Victor Biasibetti <victorbiasibetti7@gmail.com>
2023-01-17 08:20:28 -0300

    test: ensure SurveyResult present correct result on success

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -9,12 +9,12 @@ export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
     answer: faker.random.word(),
     count: faker.datatype.number(),
     percent: faker.datatype.number(),
-    isCurrentAccountAnswer: faker.datatype.boolean()
+    isCurrentAccountAnswer: true
   },{
     answer: faker.random.word(),
     count: faker.datatype.number(),
     percent: faker.datatype.number(),
-    isCurrentAccountAnswer: faker.datatype.boolean()
+    isCurrentAccountAnswer: false
   }]
 })
 
--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render, screen, waitFor } from '@testing-library/react'
 import { SurveyResult } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
-import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, mockAccountModel, mockSurveyResultModel } from '@/domain/test'
 
 const mockUseNavigate = jest.fn()
 jest.mock('react-router-dom', () => ({
@@ -14,11 +14,11 @@ type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy
 }
 
-const makeSut = (): SutTypes => {
+const makeSut = (surveyResult = mockSurveyResultModel()): SutTypes => {
   const loadSurveyResultSpy = new LoadSurveyResultSpy()
-  const setCurrentAccountMock = jest.fn()
+  loadSurveyResultSpy.surveyResult = surveyResult
   render(
-    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
       <SurveyResult loadSurveyResult={loadSurveyResultSpy}/>
     </ApiContext.Provider>
   )
@@ -40,6 +40,35 @@ describe('SurveyResult Component', () => {
     await waitFor(() => {
       screen.getByTestId('survey-result')
       expect(loadSurveyResultSpy.callsCount).toBe(1)
+    })
+  })
+  test('Should present SurveyResult data on success ', async () => {
+    const surveyResult = Object.assign(mockSurveyResultModel(), {
+      date: new Date('2022-01-10T00:00:00')
+    })
+    makeSut(surveyResult)
+    await waitFor(() => {
+      screen.getByTestId('survey-result')
+      expect(screen.getByTestId('day')).toHaveTextContent('10')
+      expect(screen.getByTestId('month')).toHaveTextContent('jan')
+      expect(screen.getByTestId('year')).toHaveTextContent('2022')
+      expect(screen.getByTestId('question')).toHaveTextContent(surveyResult.question)
+      expect(screen.getByTestId('answers').childElementCount).toBe(surveyResult.answers.length)
+
+      const answerWrap = screen.queryAllByTestId('answer-wrap')
+      expect(answerWrap[0]).toHaveClass('active')
+      expect(answerWrap[1]).not.toHaveClass('active')
+
+      const images = screen.queryAllByTestId('image')
+      expect(images[0]).toHaveAttribute('src', surveyResult.answers[0].image)
+      expect(images[0]).toHaveAttribute('alt', surveyResult.answers[0].answer)
+      expect(images[1]).toBeFalsy()
+      const answers = screen.queryAllByTestId('answer')
+      expect(answers[0]).toHaveTextContent(surveyResult.answers[0].answer)
+      expect(answers[1]).toHaveTextContent(surveyResult.answers[1].answer)
+      const percents = screen.queryAllByTestId('percent')
+      expect(percents[0]).toHaveTextContent(`${surveyResult.answers[0].percent}%`)
+      expect(percents[1]).toHaveTextContent(`${surveyResult.answers[1].percent}%`)
     })
   })
 })
