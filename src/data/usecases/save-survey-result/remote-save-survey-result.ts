95e18f5f43c59a665e58e1d8a14f9e0050f5c8d6
Victor Biasibetti <victorbiasibetti7@gmail.com>
2023-07-13 08:04:18 -0300

    refactor: rename

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -12,6 +12,7 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
     // const httpResponse =
     await this.httpClient.request({
       url: this.url,
+      body: params,
       method: "put",
     });
     // const remoteSurveyResult = httpResponse.body;
