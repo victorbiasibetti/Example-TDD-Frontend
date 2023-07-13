dc3ec5bea71e08f4bce3c3d7722e8f5d3623610f
Victor Biasibetti <victorbiasibetti7@gmail.com>
2023-07-13 08:10:46 -0300

    feat: ensure RemoteSaveSurveyResult returns AccessDeniedError on 404

--- src/data/usecases/save-survey-result/remote-save-survey-result.ts
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -16,15 +16,15 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
     });
     // const remoteSurveyResult = httpResponse.body;
     switch (httpResponse.statusCode) {
-      // case HttpStatusCode.ok:
-      //   return Object.assign({}, remoteSurveyResult, {
-      //     date: new Date(remoteSurveyResult.date),
-      //   });
+      case HttpStatusCode.ok:
+        return null;
+      // return Object.assign({}, remoteSurveyResult, {
+      //   date: new Date(remoteSurveyResult.date),
+      // });
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError();
       default:
-        // throw new UnexpectedError();
-        return null;
+        throw new UnexpectedError();
     }
   }
 }
