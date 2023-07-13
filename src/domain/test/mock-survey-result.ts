a74e1c42a4cb514deb0a391bf8b7626a8217c560
Victor Biasibetti <victorbiasibetti7@gmail.com>
2023-07-13 08:05:11 -0300

    test: ensure RemoteSaveSurveyResult call HttpClient with correct body

--- src/domain/test/mock-survey-result.ts
+++ src/domain/test/mock-survey-result.ts
@@ -1,29 +1,36 @@
-import faker from 'faker'
-import { LoadSurveyResult } from '../usecases'
+import faker from "faker";
+import { LoadSurveyResult, SaveSurveyResult } from "../usecases";
+
+export const mockSaveSurveyResultParams = (): SaveSurveyResult.Params => ({
+  answer: faker.random.word(),
+});
 
 export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
   question: faker.random.words(10),
   date: faker.datatype.datetime(),
-  answers: [{
-    image: faker.internet.url(),
-    answer: faker.random.word(),
-    count: faker.datatype.number(),
-    percent: faker.datatype.number(),
-    isCurrentAccountAnswer: true
-  },{
-    answer: faker.random.word(),
-    count: faker.datatype.number(),
-    percent: faker.datatype.number(),
-    isCurrentAccountAnswer: false
-  }]
-})
+  answers: [
+    {
+      image: faker.internet.url(),
+      answer: faker.random.word(),
+      count: faker.datatype.number(),
+      percent: faker.datatype.number(),
+      isCurrentAccountAnswer: true,
+    },
+    {
+      answer: faker.random.word(),
+      count: faker.datatype.number(),
+      percent: faker.datatype.number(),
+      isCurrentAccountAnswer: false,
+    },
+  ],
+});
 
 export class LoadSurveyResultSpy implements LoadSurveyResult {
-  callsCount = 0
-  surveyResult = mockSurveyResultModel()
+  callsCount = 0;
+  surveyResult = mockSurveyResultModel();
 
-  async load (): Promise<LoadSurveyResult.Model> {
-    this.callsCount++
-    return this.surveyResult
+  async load(): Promise<LoadSurveyResult.Model> {
+    this.callsCount++;
+    return this.surveyResult;
   }
 }
