34272e6806141b768e8df51ac5e6e8689880d53d
Victor Biasibetti <victorbiasibetti7@gmail.com>
2023-07-13 07:53:40 -0300

    feat: ensure RemoteSaveSurveyResult call remote http client with correct values

---
+++ src/data/usecases/save-survey-result/remote-save-survey-result.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpStatusCode } from "@/data/protocols/http";
+import { AccessDeniedError, UnexpectedError } from "@/domain/erros";
+import { SaveSurveyResult } from "@/domain/usecases";
+
+export class RemoteSaveSurveyResult implements SaveSurveyResult {
+  constructor(
+    private readonly url: string,
+    private readonly httpClient: HttpClient<RemoteSaveSurveyResult.Model>
+  ) {}
+
+  async save(params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
+    // const httpResponse =
+    await this.httpClient.request({
+      url: this.url,
+      method: "put",
+    });
+    // const remoteSurveyResult = httpResponse.body;
+    // switch (httpResponse.statusCode) {
+    //   case HttpStatusCode.ok:
+    //     return Object.assign({}, remoteSurveyResult, {
+    //       date: new Date(remoteSurveyResult.date),
+    //     });
+    //   case HttpStatusCode.forbidden:
+    //     throw new AccessDeniedError();
+    //   default:
+    //     throw new UnexpectedError();
+    // }
+    return null;
+  }
+}
+
+export namespace RemoteSaveSurveyResult {
+  export type Model = RemoteSaveSurveyResult;
+}
